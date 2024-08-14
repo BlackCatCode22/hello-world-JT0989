cde0eaa6064de1e848e05ea0186e72d29c00178b
JT0989 <0989275@my.scccd.edu>
2024-08-14 11:53:44 -0700

    Add files via upload
    uploaded my first C++ program

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// jT 8/14/24
+// helloWorld.cpp
+/*
+ * First program
+ * took me a cool hour or whatever
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World! " << " \nMy name is JT " << endl;
+    return 0;
+}
